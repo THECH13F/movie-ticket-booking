e616ccb33956d40e7fd6ef9f2c547c8549020914
hardik kasliwal <hardikkasli123@gmail.com>
2025-01-21 18:30:53 +0530

    test

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie Ticket Booking Dashboard</title>
+    <title>Movie Ticket Booking Dashboard </title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="main.css">
