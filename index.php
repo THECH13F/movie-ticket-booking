15aa2594c0d5a06bb35d3624b9658a26d8e2d6f4
hardik kasliwal <hardikkasli123@gmail.com>
2025-01-26 00:10:55 +0530

    test1

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (!isset($_SESSION['user_id'])) {
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
+                        <a class="nav-link" href="index.php">Home </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="About us.php">About us</a>
